ba750dfe76dc507f6ce5f25576865efc9e5b5bbb
alirmemon37 <alirmemona@gmail.com>
2023-08-30 00:26:11 +0530

    RED: moveBackward in all directions

--- tests/chandrayaan.test.js
+++ tests/chandrayaan.test.js
@@ -9,13 +9,23 @@ describe("Chandrayaan 3 should", () => {
     ${"S"}    | ${"f"}   | ${[0, -1, 0]}
     ${"W"}    | ${"f"}   | ${[-1, 0, 0]}
     ${"D"}    | ${"f"}   | ${[0, 0, -1]}
+    ${"N"}    | ${"b"}   | ${[0, -1, 0]}
+    ${"E"}    | ${"b"}   | ${[-1, 0, 0]}
+    ${"U"}    | ${"b"}   | ${[0, 0, -1]}
+    ${"S"}    | ${"b"}   | ${[0, 1, 0]}
+    ${"W"}    | ${"b"}   | ${[1, 0, 0]}
+    ${"D"}    | ${"b"}   | ${[0, 0, 1]}
   `(
     "when facing $direction moving $movement by 1 should change the position correctly",
     ({ direction, movement, expected }) => {
       const initialPosition = [0, 0, 0];
       const initialDirection = direction;
 
-      expect(moveForward(initialPosition, initialDirection, movement)).toEqual(expected);
+      if (movement === "f") {
+        expect(moveForward(initialPosition, initialDirection, movement)).toEqual(expected);
+      } else {
+        expect(moveBackward(initialPosition, initialDirection, movement)).toEqual(expected);
+      }
     }
   );
 });
