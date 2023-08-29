d42400a6ed39299ec897a26f9fade9d9c0217e28
alirmemon37 <alirmemona@gmail.com>
2023-08-30 00:05:41 +0530

    GREEN: movingForward in N, E & U

--- chandrayaan.js
+++ chandrayaan.js
@@ -1,5 +1,7 @@
 const moveForward = (position, direction) => {
-  return [0, 1, 0];
+  if (direction === "N") return [0, 1, 0];
+  if (direction === "E") return [1, 0, 0];
+  if (direction === "U") return [0, 0, 1];
 };
 
 module.exports = moveForward;
