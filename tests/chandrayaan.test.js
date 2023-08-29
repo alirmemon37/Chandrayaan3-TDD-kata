9404c89157364ca8a55f8421c147b6b5447faf58
alirmemon37 <alirmemona@gmail.com>
2023-08-30 00:03:48 +0530

    RED: movingForward in N, E & U

--- tests/chandrayaan.test.js
+++ tests/chandrayaan.test.js
@@ -1,10 +1,24 @@
 const moveForward = require("../chandrayaan");
 
 describe("Chandrayaan 3 should", () => {
-  it("when facing north moving forward by 1 should increase the y coordinate by 1", () => {
+  it("when facing N moving forward by 1 should increase the y coordinate by 1", () => {
     const initialPosition = [0, 0, 0];
     const initialDirection = "N";
 
     expect(moveForward(initialPosition, initialDirection)).toEqual([0, 1, 0]);
+  });
+
+  it("when facing E moving forward by 1 should increase the x coordinate by 1", () => {
+    const initialPosition = [0, 0, 0];
+    const initialDirection = "E";
+
+    expect(moveForward(initialPosition, initialDirection)).toEqual([1, 0, 0]);
+  });
+
+  it("when facing Up moving forward by 1 should increase the z coordinate by 1", () => {
+    const initialPosition = [0, 0, 0];
+    const initialDirection = "U";
+
+    expect(moveForward(initialPosition, initialDirection)).toEqual([0, 0, 1]);
   });
 });
