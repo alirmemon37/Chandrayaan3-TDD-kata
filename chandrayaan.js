66ebf1771988e898871227f69ddd121e07bed03e
alirmemon37 <alirmemona@gmail.com>
2023-08-30 00:16:17 +0530

    GREEN: moveForward in all directions

--- chandrayaan.js
+++ chandrayaan.js
@@ -4,6 +4,9 @@ const moveForward = (position, direction) => {
   if (direction === "N") return [x, y + 1, z];
   if (direction === "E") return [x + 1, y, z];
   if (direction === "U") return [x, y, z + 1];
+  if (direction === "S") return [x, y - 1, z];
+  if (direction === "W") return [x - 1, y, z];
+  if (direction === "D") return [x, y, z - 1];
 };
 
 module.exports = moveForward;
