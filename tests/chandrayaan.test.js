18870096088865884aaf356bb3716e0b02eef751
alirmemon37 <alirmemona@gmail.com>
2023-08-29 23:55:53 +0530

    RED: moveForward when facing north

---
+++ tests/chandrayaan.test.js
@@ -0,0 +1,8 @@
+describe("Chandrayaan 3 should", () => {
+  it("when facing north moving forward by 1 should increase the y coordinate by 1", () => {
+    const initialPosition = [0, 0, 0];
+    const initialDirection = "N";
+
+    expect(moveForward(initialPosition, initialDirection)).toEqual([0, 1, 0]);
+  });
+});
